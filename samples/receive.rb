fac26d84f30abf2f448c59cce558e6740fa5355c
64you <n.ojep@gmail.com>
2023-08-23 09:49:58 +0900

    communicator、経路探索の追加

---
+++ samples/receive.rb
@@ -0,0 +1,14 @@
+begin
+  threads = []
+  receiver = Communicator::Receiver.new
+
+  threads << Thread.start do
+    signals = receiver.receive
+    message = receiver.get_message(signals)
+    puts "receive: #{message}"
+  end
+
+  threads.each{|t| t.join }
+ensure
+  receiver.disconnect
+end
